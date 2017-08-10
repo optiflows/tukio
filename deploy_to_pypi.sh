43b4a2944883a18bd3f2f2fdec5f8763c73891e2
Guillaume Desvé <guillaume.desve@surycat.com>
2017-08-10 16:35:45 +0200

    Migrate to circle2.0

--- deploy_to_pypi.sh
+++ deploy_to_pypi.sh
@@ -1,11 +1,11 @@
-echo -e "[distutils]
-index-servers=pypi
+VERSION_TAG=$1
 
-[pypi]
-username = $PYPI_USER
-password = $PYPI_PASSWORD
-" > ~/.pypirc
-
-echo $CIRCLE_TAG > VERSION.txt
-python setup.py sdist
-twine upload dist/tukio-$CIRCLE_TAG.tar.gz
+if [ ! -z $VERSION_TAG ]; then
+  pipenv lock -r > requirements.txt
+  echo $VERSION_TAG > VERSION.txt
+  python setup.py sdist
+  twine upload -u $PYPI_USER -p $PYPI_PASSWORD dist/tukio-$VERSION_TAG.tar.gz
+else
+  echo "No VERSION_TAG defined, skipping packaging and upload."
+  exit 1;
+fi
