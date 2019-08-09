21d97615bfd53c7bafc38a804d3d2e4b179c0966
Guillaume Desvé <dev@gdesve.dev>
2019-08-09 14:20:47 +0200

    Setup code cleanup

--- deploy_to_pypi.sh
+++ deploy_to_pypi.sh
@@ -1,11 +1,10 @@
 VERSION_TAG=$1
 
 if [ ! -z $VERSION_TAG ]; then
-  pipenv lock -r > requirements.txt
-  echo $VERSION_TAG > VERSION.txt
-  python setup.py sdist
-  twine upload -u $PYPI_USER -p $PYPI_PASSWORD dist/tukio-$VERSION_TAG.tar.gz
+    echo $VERSION_TAG > VERSION.txt
+    python setup.py sdist
+    twine upload -u $PYPI_USER -p $PYPI_PASSWORD dist/tukio-$VERSION_TAG.tar.gz
 else
-  echo "No VERSION_TAG defined, skipping packaging and upload."
-  exit 1;
+    echo "No VERSION_TAG defined, skipping packaging and upload."
+    exit 1;
 fi
